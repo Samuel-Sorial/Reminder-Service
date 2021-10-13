5c28170a6623c3befa4c2c5cf493897b5741a187
Samuel-Sorial <samuelmagedsorial@gmail.com>
2021-10-13 02:44:41 +0200

    Adding environments to manage env variables, starting websocket server

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-console.log('Hello World!')
+import { WebSocketServer } from "./WebSocket/Server";
+import { PORT } from "./config/environment";
+
+WebSocketServer.startServer(PORT);
+WebSocketServer.addMessageListener((message) =>
+    console.log(`Hello from websocket server! You just sent: ${message}`)
+);
