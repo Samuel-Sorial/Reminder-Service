6b77cbb64c68e2e345ec933c6066e8fc069a2a93
Samuel-Sorial <samuelmagedsorial@gmail.com>
2021-10-14 03:23:25 +0200

    Adding skeleton for Reminder class

--- src/Reminder/Reminder.ts
+++ src/Reminder/Reminder.ts
@@ -1,3 +1,5 @@
+import { DateUtils } from "../Date/DateUtils";
+
 export class Reminder {
     public readonly date: Date;
     public readonly message: string;
@@ -5,6 +7,16 @@ export class Reminder {
     constructor(date: Date, message: string) {
         this.date = date;
         this.message = message;
+    }
+
+    private static validateReminder(reminder: any) {
+        throw Error("Not implemented yet.");
+    }
+
+    public static fromString(plainReminder: string) {
+        const jsonReminder = JSON.parse(plainReminder);
+        this.validateReminder(jsonReminder);
+        return new Reminder(jsonReminder.date, jsonReminder.message);
     }
 
     public notify(): void {
