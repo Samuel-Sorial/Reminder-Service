3208557cd6c7c7d8eab8edf4539eb0eac659a9a2
Samuel-Sorial <samuelmagedsorial@gmail.com>
2021-10-14 03:36:19 +0200

    Adding test cases for Reminder fromString

---
+++ src/Reminder/Reminder.test.ts
@@ -0,0 +1,19 @@
+import { Reminder } from "./Reminder";
+
+describe("create reminder from string", function () {
+    it("fails given invalid json string", function () {
+        const createReminder = () => Reminder.fromString("");
+        expect(createReminder).toThrowError(SyntaxError);
+    });
+
+    it("fails when missing date", function () {
+        const createReminder = () => Reminder.fromString(`{"message":"test"}`);
+        expect(createReminder).toThrowError(RangeError);
+    });
+
+    it("fails when missing message ", function () {
+        const createReminder = () =>
+            Reminder.fromString(`{"date":"${new Date().toISOString()}"}`);
+        expect(createReminder).toThrowError(RangeError);
+    });
+});
