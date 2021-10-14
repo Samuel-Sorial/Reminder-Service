42c6871d9ebf2fccc8f9aa3067f46d92072cfacd
Samuel-Sorial <samuelmagedsorial@gmail.com>
2021-10-14 02:28:58 +0200

    Testing RabbitMQ setup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { WebSocketServer } from "./WebSocket/Server";
-import { PORT } from "./Config/environment";
+import { MessageBroker } from "./MessageBroker/MessageBroker";
+import { PORT, MESSAGE_BROKER_CONNECTION } from "./Config/environment";
 
+if (!MESSAGE_BROKER_CONNECTION) {
+    throw new Error(
+        "Can not start service without specifying Message Broker connection string."
+    );
+}
+MessageBroker.connect(MESSAGE_BROKER_CONNECTION);
 WebSocketServer.startServer(PORT);
-WebSocketServer.addMessageListener((message) =>
-    console.log(`Hello from websocket server! You just sent: ${message}`)
-);
