719633e71be0d1a8524652ec1f5a8cfc8c12a207
Samuel-Sorial <samuelmagedsorial@gmail.com>
2021-10-14 04:06:25 +0200

    Adding get millseconds & test cases

--- src/Date/DateUtils.test.ts
+++ src/Date/DateUtils.test.ts
@@ -13,3 +13,39 @@ describe("Validate date", function () {
         expect(isValidDate).toBe(false);
     });
 });
+
+describe("Subtract two dates", function () {
+    it("returns correct value when first date greater than second", function () {
+        const firstDate = new Date();
+        const secondDate = new Date();
+        const DIFF_IN_MINUTES = 5;
+        secondDate.setMinutes(firstDate.getMinutes() - DIFF_IN_MINUTES);
+        const durationMillSeconds = DateUtils.subtractInMillSeconds(
+            firstDate,
+            secondDate
+        );
+
+        expect(durationMillSeconds).toBe(DIFF_IN_MINUTES * 60 * 1000);
+    });
+
+    it("returns correct value when first date less than second", function () {
+        const firstDate = new Date();
+        const secondDate = new Date();
+        const DIFF_IN_MINUTES = 5;
+        secondDate.setMinutes(firstDate.getMinutes() + 5);
+        const durationMillSeconds = DateUtils.subtractInMillSeconds(
+            firstDate,
+            secondDate
+        );
+
+        expect(durationMillSeconds).toBe(-DIFF_IN_MINUTES * 60 * 1000);
+    });
+
+    it("returns 0 when same date", function () {
+        const date = new Date();
+
+        const durationMillSeconds = DateUtils.subtractInMillSeconds(date, date);
+
+        expect(durationMillSeconds).toBe(0);
+    });
+});
--- src/Date/DateUtils.ts
+++ src/Date/DateUtils.ts
@@ -2,4 +2,8 @@ export class DateUtils {
     public static isValidDate(date: Date): boolean {
         return !isNaN(date.getTime());
     }
+
+    public static subtractInMillSeconds(date1: Date, date2: Date): number {
+        return date1.getTime() - date2.getTime();
+    }
 }
