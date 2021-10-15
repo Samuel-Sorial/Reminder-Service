5455a382e594747143c810b5233ed5320a445109
Samuel-Sorial <samuelmagedsorial@gmail.com>
2021-10-15 04:38:50 +0200

    Implementing move next group to next observer

--- src/Reminder/Observer/LongTermObserver.ts
+++ src/Reminder/Observer/LongTermObserver.ts
@@ -1,14 +1,27 @@
 import { Reminder } from "../Reminder";
 import { Observer } from "./Observer";
 import { Database } from "../../Database/Database";
+import { DateUtils } from "../../Date/DateUtils";
 
 export class LongTermObserver implements Observer {
-    private static getListName(date: Date): string {
-        throw new Error("Not implemented yet");
+    private static getNearestGroup(date: Date): string {
+        return DateUtils.floorByMinutes(date, 1).toISOString();
+    }
+
+    static async moveNextGroupToNextObserver() {
+        const listName = DateUtils.roundByMinutes(new Date(), 1).toISOString();
+        const reminders = await Database.getListElements(listName);
+        reminders.forEach((reminder) => {
+            const parsedReminder = Reminder.fromString(reminder);
+
+            parsedReminder.notify();
+        });
+        await Database.removeList(listName);
+        return { listName, totalReminders: reminders.length };
     }
 
     async sendReminder(reminder: Reminder) {
-        const listName = LongTermObserver.getListName(reminder.date);
+        const listName = LongTermObserver.getNearestGroup(reminder.date);
         await Database.addToList(listName, reminder.toString());
     }
 }
