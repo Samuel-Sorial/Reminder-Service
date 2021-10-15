896f519c053a803dbb699821e16a0f6a09b8aff1
Samuel-Sorial <samuelmagedsorial@gmail.com>
2021-10-15 04:37:57 +0200

    Reducing short term observer interval

--- src/Reminder/Observer/ShortTermObserver.ts
+++ src/Reminder/Observer/ShortTermObserver.ts
@@ -4,7 +4,7 @@ import { MessageBroker } from "../../MessageBroker/MessageBroker";
 import { DateUtils } from "../../Date/DateUtils";
 
 export class ShortTermObserver implements Observer {
-    public static readonly INTERVAL_MILLSECONDS = 5 * 60 * 1000;
+    public static readonly INTERVAL_MILLSECONDS = 3 * 60 * 1000;
     public static readonly QUEUE_NAME = "reminder";
 
     public static onMessage(message: string) {
@@ -20,7 +20,7 @@ export class ShortTermObserver implements Observer {
             throw new Error("Wrong observer!");
         }
         await MessageBroker.delayedPublish(
-            JSON.stringify(reminder),
+            reminder.toString(),
             ShortTermObserver.QUEUE_NAME,
             durationInMillSeconds
         );
