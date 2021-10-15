3175bed5f29d83aa3829989cb163793a4b33d94a
Samuel-Sorial <samuelmagedsorial@gmail.com>
2021-10-15 02:46:07 +0200

    Implementing sendReminder on long term observer

--- src/Reminder/Observer/LongTermObserver.ts
+++ src/Reminder/Observer/LongTermObserver.ts
@@ -1,6 +1,14 @@
 import { Reminder } from "../Reminder";
 import { Observer } from "./Observer";
+import { Database } from "../../Database/Database";
 
 export class LongTermObserver implements Observer {
-    sendReminder(reminder: Reminder): void {}
+    private static getListName(date: Date): string {
+        throw new Error("Not implemented yet");
+    }
+
+    async sendReminder(reminder: Reminder) {
+        const listName = LongTermObserver.getListName(reminder.date);
+        await Database.addToList(listName, reminder.toString());
+    }
 }
