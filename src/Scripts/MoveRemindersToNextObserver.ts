c9780b3d2f0876659cd79fffc50f85b466ea3805
Samuel-Sorial <samuelmagedsorial@gmail.com>
2021-10-17 03:48:05 +0200

    Closing connection & injecting dependencies

--- src/Scripts/MoveRemindersToNextObserver.ts
+++ src/Scripts/MoveRemindersToNextObserver.ts
@@ -2,6 +2,7 @@ import { MessageBroker } from "../MessageBroker/MessageBroker";
 import { Database } from "../Database/Database";
 import { MESSAGE_BROKER_URL, DATABASE_URL } from "../Config/Environment";
 import { LongTermObserver } from "../Reminder/Observer/LongTermObserver";
+import { ShortTermObserver } from "../Reminder/Observer/ShortTermObserver";
 
 export async function moveRemindersToNextObserver() {
     if (!MESSAGE_BROKER_URL) {
@@ -13,12 +14,16 @@ export async function moveRemindersToNextObserver() {
         throw new Error("Can not start service without specifying DB url.");
     }
     await MessageBroker.connect(MESSAGE_BROKER_URL);
+    ShortTermObserver.useEngine(MessageBroker);
     Database.startServer(DATABASE_URL);
+    LongTermObserver.useEngine(Database);
     const { totalReminders, listName } =
         await LongTermObserver.moveNextGroupToNextObserver();
     console.log(`Successfully ran Move reminders script at ${new Date().toISOString()}: 
         - Moved ${totalReminders} reminder(s)
         - Removed db list: ${listName}`);
+    await MessageBroker.closeConnection();
+    await Database.closeServer();
 }
 
 moveRemindersToNextObserver();
