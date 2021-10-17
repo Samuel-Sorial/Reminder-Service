084800db18a8c02b9188bd65fcfc1586c0dda28f
Samuel-Sorial <samuelmagedsorial@gmail.com>
2021-10-17 03:46:49 +0200

    Updating onMessage to wait promise of callback

--- src/WebSocket/Server.ts
+++ src/WebSocket/Server.ts
@@ -13,11 +13,13 @@ export class WebSocketServer {
         console.log(`Started successfully WebSocket server on port ${port}`);
     }
 
-    public static addMessageListener(callback: (message: string) => void) {
+    public static addMessageListener(
+        callback: (message: string) => Promise<void>
+    ) {
         this.server.on("connection", (socket: WebSocket) => {
-            socket.on("message", (message: RawData) => {
+            socket.on("message", async (message: RawData) => {
                 try {
-                    callback(message.toString());
+                    await callback(message.toString());
                 } catch (error) {
                     socket.send(
                         `Invalid reminder, a valid reminder should be like:
