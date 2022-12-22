8418094bd581833318b9b26e6e823d250b30daea
Samuel Sorial <samuelmagedsorial@gmail.com>
2022-12-22 21:51:59 +0200

    feat: adding more redis test cases

--- src/database/redits-database.test.ts
+++ src/database/redits-database.test.ts
@@ -1,17 +1,27 @@
 import { RedisDatabase } from "./redis-database";
 import * as redisMock from "redis-mock";
+import { logger } from "../logger";
 
+const listNames = {
+    add: "add-test-list",
+    get: "get-test-list",
+    remove: "remove-test-list",
+    nonExisting: "non-existing-list",
+};
 describe("RedisDatabase", () => {
     let redisDatabase: RedisDatabase;
     let mockClient: redisMock.RedisClient;
+    beforeAll(() => {
+        logger.silent = true;
+    });
+    afterAll(() => {
+        logger.silent = false;
+    });
 
     beforeEach(() => {
         mockClient = redisMock.createClient();
-        try {
-            redisDatabase = new RedisDatabase("fake-url");
-            // Override the private `client` property with the mock client
-            redisDatabase["client"] = mockClient;
-        } catch (err) {}
+        redisDatabase = new RedisDatabase("");
+        redisDatabase["client"] = mockClient;
     });
 
     afterEach(() => {
@@ -20,10 +30,47 @@ describe("RedisDatabase", () => {
 
     describe("addToList", () => {
         it("should add an element to the list", async () => {
-            await redisDatabase.addToList("my-list", "element1");
-            expect(await redisDatabase.getListElements("my-list")).toEqual([
+            await redisDatabase.addToList(listNames.add, "element1");
+            expect(await redisDatabase.getListElements(listNames.add)).toEqual([
                 "element1",
             ]);
+        });
+    });
+
+    describe("getListElements", () => {
+        it("should return the elements in the list", async () => {
+            await redisDatabase.addToList(listNames.get, "element1");
+            await redisDatabase.addToList(listNames.get, "element2");
+            await redisDatabase.addToList(listNames.get, "element3");
+
+            const elements = await redisDatabase.getListElements(listNames.get);
+            expect(elements).toEqual(
+                expect.arrayContaining(["element1", "element2", "element3"])
+            );
+        });
+
+        it("should return an empty array if the list does not exist", async () => {
+            const elements = await redisDatabase.getListElements(
+                listNames.nonExisting
+            );
+            expect(elements).toEqual([]);
+        });
+    });
+
+    describe("removeList", () => {
+        it("should remove an existing list", async () => {
+            await redisDatabase.addToList(listNames.remove, "element1");
+            await redisDatabase.removeList(listNames.remove);
+            expect(
+                await redisDatabase.getListElements(listNames.remove)
+            ).toEqual([]);
+        });
+
+        it("should complete even if the list does not exist", async () => {
+            await redisDatabase.removeList(listNames.remove);
+            expect(
+                await redisDatabase.getListElements(listNames.remove)
+            ).toEqual([]);
         });
     });
 });
