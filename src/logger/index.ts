5bacb45fc33a0d6f76ef4e196efde6c71ea0bd7d
Samuel Sorial <samuelmagedsorial@gmail.com>
2022-12-22 22:50:27 +0200

    fix: cron job script name

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,8 +1,5 @@
 import { createLogger, transports } from "winston";
 
 export const logger = createLogger({
-    transports: [
-        new transports.File({ level: "error", filename: "logs/error.log" }),
-        new transports.Console(),
-    ],
+    transports: [new transports.Console()],
 });
